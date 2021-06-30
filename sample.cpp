de8fb7c82129a4f40aeff100a47b9acba3e5ee8c
gen <gentygengen@gmail.com>
2021-06-30 19:09:19 +0900

    create sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+int sum(int, int);
+
 int main()
 {
+    int a = 5; int b = 3;
     std::cout << "Hello World!" << std::endl;
     return 0;
+}
+
+int sum(int a, int b)
+{
+    return a+b;
 }

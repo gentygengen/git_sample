5ca0caece2848cf9459148ff6bf0bdd83f0d9af3
gen <gentygengen@gmail.com>
2021-07-28 22:25:47 +0900

    “kadai.cpp”

---
+++ .vscode/kadai.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int a, b;
+    std::cout << "a = ";
+    std::cin >> a;
+    std::cout << "b = ";
+    std::cin >> b;
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "a - b = " << a - b << std::endl;
+    std::cout << "a * b = " << a * b << std::endl;
+    std::cout << "a / b = " << a / b << std::endl;
+
+    return 0;
+}
